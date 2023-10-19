cfd4f3eeb8c8782d3c3d113512c8a0b56375c3b0
y2jesan <ymjesanlemon@gmail.com>
2023-10-19 17:36:36 +0600

    more blogs

--- app/api/blog/[id]/route.js
+++ app/api/blog/[id]/route.js
@@ -2,41 +2,83 @@ import { NextResponse } from 'next/server';
 
 import connectMongoDB from '@/lib/mongodb';
 import Blog from '@/models/Blog';
+import { ObjectId } from 'mongodb';
 
 export async function PUT(request, { params }) {
-    const { id } = params;
-    const {
-        newTitle: title,
-        newBlogContent: blog_content,
-        newAuthorName: author_name,
-    } = await request.json();
+  const { id } = params;
+  const { newTitle: title, newBlogContent: blog_content, newAuthorName: author_name } = await request.json();
 
-    await connectMongoDB();
+  await connectMongoDB();
 
-    try {
-        const updatedBlog = await Blog.findByIdAndUpdate(
-            id,
-            { title, blog_content, author_name },
-            { new: true }
-        );
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(id, { title, blog_content, author_name }, { new: true });
 
-        if (!updatedBlog) {
-            return NextResponse.error('Blog not found', { status: 404 });
-        }
-
-        return NextResponse.json(
-            { message: 'Blog updated', blogPost: updatedBlog },
-            { status: 200 }
-        );
-    } catch (error) {
-        return NextResponse.error('Failed to update blog', { status: 500 });
+    if (!updatedBlog) {
+      return NextResponse.error('Blog not found', { status: 404 });
     }
+
+    return NextResponse.json({ message: 'Blog updated', blogPost: updatedBlog }, { status: 200 });
+  } catch (error) {
+    return NextResponse.error('Failed to update blog', { status: 500 });
+  }
 }
 
 export async function GET(request, { params }) {
-    const { id } = params;
+  const { id } = params;
 
-    await connectMongoDB();
-    const blog = await Blog.findOne({ _id: id });
-    return NextResponse.json({ blog }, { status: 200 });
+  await connectMongoDB();
+  const blog = await Blog.aggregate([
+    {
+      $match: { _id: ObjectId(id) },
+    },
+    {
+      $lookup: {
+        from: 'blogs',
+        let: { blogId: '$_id' },
+        pipeline: [
+          {
+            $match: {
+              $and: [
+                {
+                  $expr: {
+                    $ne: ['$_id', '$$blogId'],
+                  },
+                },
+                {
+                  $expr: {
+                    $eq: ['$active', true],
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $sample: { size: 2 },
+          },
+        ],
+        as: 'more',
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        title: 1,
+        blog_cover_img: 1,
+        author_name: 1,
+        read_time: 1,
+        blog_content: 1,
+        created_at: 1,
+        active: 1,
+        more: {
+          _id: 1,
+          title: 1,
+          blog_cover_img: 1,
+          author_name: 1,
+          active: 1,
+          created_at: 1,
+        },
+      },
+    },
+  ]);
+  return NextResponse.json({ blog: blog[0] }, { status: 200 });
 }
