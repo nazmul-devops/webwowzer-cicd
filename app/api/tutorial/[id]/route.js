3bbd1ffade3e570e5e3c7dd9757f8fa503a08922
clevermiraz <miraz.onesoft@gmail.com>
2023-10-23 15:44:42 +0600

    🚀 `tutorial` CRUD API Implement

---
+++ app/api/tutorial/[id]/route.js
@@ -0,0 +1,87 @@
+import { NextResponse } from 'next/server';
+
+import connectMongoDB from '@/lib/mongodb';
+import { tutorialSchema } from '@/lib/validation';
+import Tutorial from '@/models/Tutorial';
+
+export async function PUT(request, { params }) {
+    const { id } = params;
+    const { title, thumbnail_img, video_url, duration } = await request.json();
+
+    try {
+        const validateData = tutorialSchema.parse({
+            title,
+            thumbnail_img,
+            video_url,
+            duration,
+        });
+        await connectMongoDB();
+
+        const updatedTutorial = await Tutorial.findByIdAndUpdate(id, validateData, { new: true });
+
+        if (!updatedTutorial) {
+            return NextResponse.error('Tutorial not found', { status: 404 });
+        }
+
+        return NextResponse.json(
+            { message: 'Tutorial updated', tutorials: updatedTutorial },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
+
+export async function GET(request, { params }) {
+    const { id } = params;
+
+    await connectMongoDB();
+    const tutorial = await Tutorial.findOne({ _id: id });
+
+    return NextResponse.json({ tutorial }, { status: 200 });
+}
+
+export async function PATCH(request, { params }) {
+    const { id } = params;
+    const { active } = await request.json();
+
+    await connectMongoDB();
+
+    try {
+        const updatedTutorial = await Tutorial.findByIdAndUpdate(id, { active }, { new: true });
+
+        if (!updatedTutorial) {
+            return NextResponse.error('Tutorial not found', { status: 404 });
+        }
+
+        return NextResponse.json(
+            { message: 'Active status updated', tutorial: updatedTutorial },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.error('Failed to update active status', { status: 500 });
+    }
+}
+
+export async function DELETE(request, { params }) {
+    const { id } = params;
+
+    // Connect to your MongoDB database
+    await connectMongoDB();
+
+    try {
+        // Use MongoDB's `findByIdAndDelete` to delete
+        const deletedTutorial = await Tutorial.findByIdAndDelete(id);
+
+        if (!deletedTutorial) {
+            return NextResponse.error('Tutorial not found', { status: 404 });
+        }
+
+        return NextResponse.json(
+            { message: 'Tutorial deleted', blogPost: deletedTutorial },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.error('Failed to delete the tutorial', { status: 500 });
+    }
+}
---
+++ app/api/tutorial/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+
+import connectMongoDB from '@/lib/mongodb';
+import { tutorialSchema } from '@/lib/validation';
+import Tutorial from '@/models/Tutorial';
+
+export async function GET() {
+    await connectMongoDB();
+
+    const tutorials = await Tutorial.find({ active: true });
+
+    return NextResponse.json({ tutorials }, { status: 200 });
+}
+
+export async function POST(request) {
+    try {
+        // Extract data from the request body
+        const requestData = await request.json();
+
+        // Validate the request data against the schema
+        const validatedData = tutorialSchema.parse(requestData);
+
+        await connectMongoDB();
+
+        // Create a new blog post
+        const newTutorial = new Tutorial(validatedData);
+        const savedTutorial = await newTutorial.save();
+
+        return NextResponse.json({
+            success: true,
+            tutorial: savedTutorial,
+            message: 'Tutorial created successfully',
+        });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
