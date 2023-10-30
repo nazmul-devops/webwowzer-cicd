9f3286d0c935224c123b247f95c3036bc4b540f4
clevermiraz <miraz.onesoft@gmail.com>
2023-10-30 11:24:04 +0600

    Admin Page Profile Section Started.

--- components/admin/AdminHeader.jsx
+++ components/admin/AdminHeader.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -15,11 +15,13 @@ import '../../public/admin/assets/vendor/simple-datatables/style.css';
 import '../../public/admin/assets/css/style.css';
 
 export default function AdminHeader() {
+    const session = useSession();
+
     const router = useRouter();
 
     const handleLoggedOut = () => {
         signOut({
-            redirect: false,
+            redirect: true,
         }).then(() => {
             router.push('/login');
             toast.success('Logged out successfully!');
@@ -44,111 +46,34 @@ export default function AdminHeader() {
 
             <nav className="header-nav ms-auto">
                 <ul className="d-flex align-items-center">
-                    <li className="nav-item d-block d-lg-none">
-                        <a className="nav-link nav-icon search-bar-toggle " href="#">
-                            <i className="bi bi-search" />
-                        </a>
-                    </li>
-
-                    <li className="nav-item dropdown">
-                        <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                            <i className="bi bi-bell" />
-                            <span className="badge bg-primary badge-number">4</span>
-                        </a>
-
-                        <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                            <li className="dropdown-header">
-                                You have 4 new notifications
-                                <a href="#">
-                                    <span className="badge rounded-pill bg-primary p-2 ms-2">
-                                        View all
-                                    </span>
-                                </a>
-                            </li>
-
-                            <li className="notification-item">
-                                <i className="bi bi-exclamation-circle text-warning" />
-                                <div>
-                                    <h4>Lorem Ipsum</h4>
-                                    <p>Quae dolorem earum veritatis oditseno</p>
-                                    <p>30 min. ago</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <hr className="dropdown-divider" />
-                            </li>
-
-                            <li className="notification-item">
-                                <i className="bi bi-x-circle text-danger" />
-                                <div>
-                                    <h4>Atque rerum nesciunt</h4>
-                                    <p>Quae dolorem earum veritatis oditseno</p>
-                                    <p>1 hr. ago</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <hr className="dropdown-divider" />
-                            </li>
-
-                            <li className="notification-item">
-                                <i className="bi bi-check-circle text-success" />
-                                <div>
-                                    <h4>Sit rerum fuga</h4>
-                                    <p>Quae dolorem earum veritatis oditseno</p>
-                                    <p>2 hrs. ago</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <hr className="dropdown-divider" />
-                            </li>
-
-                            <li className="notification-item">
-                                <i className="bi bi-info-circle text-primary" />
-                                <div>
-                                    <h4>Dicta reprehenderit</h4>
-                                    <p>Quae dolorem earum veritatis oditseno</p>
-                                    <p>4 hrs. ago</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <hr className="dropdown-divider" />
-                            </li>
-                            <li className="dropdown-footer">
-                                <a href="#">Show all notifications</a>
-                            </li>
-                        </ul>
-                    </li>
-
                     <li className="nav-item dropdown pe-3">
                         <a
                             className="nav-link nav-profile d-flex align-items-center pe-0"
                             href="#"
                             data-bs-toggle="dropdown"
                         >
-                            {/* <img src="assets/img/profile-img.jpg" alt="Profile" className="rounded-circle" /> */}
                             <span className="d-none d-md-block dropdown-toggle ps-2">
-                                user Name
+                                {`${session?.data?.user?.first_name} ${session?.data?.user?.last_name}`}
                             </span>
                         </a>
 
                         <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                             <li className="dropdown-header">
-                                <h6>Admin Name</h6>
-                                <span>Admininstrator</span>
+                                <h6>{`${session?.data?.user?.first_name} ${session?.data?.user?.last_name}`}</h6>
+                                <span>{session?.data?.user?.role}</span>
                             </li>
                             <li>
                                 <hr className="dropdown-divider" />
                             </li>
 
                             <li>
-                                <a className="dropdown-item d-flex align-items-center">
+                                <Link
+                                    href="/admin/account-settings"
+                                    className="dropdown-item d-flex align-items-center"
+                                >
                                     <i className="bi bi-person" />
                                     <span>My Profile</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <hr className="dropdown-divider" />
